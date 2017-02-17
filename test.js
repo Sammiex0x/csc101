772710d5a0590aa5b201274fe53a5d0ea951f5c6
sammiex0x <sammiex0x.sammiex0x@hotmail.com>
2017-02-16 21:13:33 -0800

    button homework

--- test.js
+++ test.js
@@ -14,3 +14,58 @@ var currTotal = document.getElementById("tot").innerHTML;
 
     document.getElementById("tot").innerHTML = newTotal;
 }
+var start = 3;
+
+function bac() {
+
+console.log("I'm a Bacon lover");
+
+var currTotal = document.getElementById("tot").innerHTML;
+    if (isNaN(currTotal)) {
+        currTotal = start;
+    }
+
+
+    var newTotal = 3 + +currTotal;
+
+    document.getElementById("tot").innerHTML = newTotal;
+}
+
+var start = 3;
+
+function pep() {
+
+console.log("I'm a pep lover");
+
+var currTotal = document.getElementById("tot").innerHTML;
+    if (isNaN(currTotal)) {
+        currTotal = start;
+    }
+
+
+    var newTotal = 1 + +currTotal;
+
+    document.getElementById("tot").innerHTML = newTotal;
+}
+
+var start = 3;
+
+function ham() {
+
+console.log("I'm a ham lover");
+
+var currTotal = document.getElementById("tot").innerHTML;
+    if (isNaN(currTotal)) {
+        currTotal = start;
+    }
+
+
+    var newTotal = 2 + +currTotal;
+
+    document.getElementById("tot").innerHTML = newTotal;
+}
+
+function clear() {
+      document.getElementById("tot").innerHTML = "0";
+
+}
